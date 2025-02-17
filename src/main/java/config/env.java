e11321c9b90825aa9704fcd533ce75a9b9b26ed8
NMAtech <nadindramaulanaaziz@gmail.com>
2025-02-17 19:23:32 +0700

    set up project

---
+++ src/main/java/config/env.java
@@ -0,0 +1,8 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+
+public class env {
+    protected WebDriver driver;
+    public String website_url = "https://nmatech.github.io/QUnit_RegistApp/";
+}
